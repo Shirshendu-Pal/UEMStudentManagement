b97e5565beb9cf3ec314c7511f4f9e4c06dba666
unknown <shirshendupal9@gmail.com>
2021-09-13 12:58:02 +0530

    code updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
-import { StudentService } from './student.service';
+
+
 
 @Component({
   selector: 'app-root',
@@ -10,34 +10,13 @@ import { StudentService } from './student.service';
 export class AppComponent {
   title = 'StudentDashboard';
   studentDetails: any;
+ 
 
 
-  constructor(private studentService: StudentService) {
-    this.getStudentsDetails();
+  constructor() {
+     
+   
   }
-  register(registerForm: NgForm){
-      this.studentService.registerStudent(registerForm.value).subscribe(
-        (resp) => {
-          console.log(resp);
-          registerForm.reset();
-          this.getStudentsDetails();
-          
-        },(err) => {
-          console.log(err);
-          
-        }
-      );
-  }
-  getStudentsDetails(){
-    this.studentService.getStduents().subscribe(
-      (resp) => {
-        console.log(resp);
-        this.studentDetails = resp;
-        
-      },(err) => {
-        console.log(err);
-        
-      }
-    );
-  }
+ 
+ 
 }
